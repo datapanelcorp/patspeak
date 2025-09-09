4c96f8c69fb25390d0c6d91cbc64d04324dd8706
Jaron McDaniel <jmcdaniel@datapanel.com>
2025-09-09 12:13:04 -0500

    updated roi.py to use gpiozero for GPIO eliminating the need to run with sudo, improved multimeter detection

--- roi.py
+++ roi.py
@@ -1,30 +1,36 @@
 #pscp .\roi.py j@picando:/home/j
-#pip install pyserial python-can
-
+#pip install pyserial python-can gpiozero
+#sudo ip link set can0 up type can bitrate 250000
+#sudo ip link set can1 up type can bitrate 250000
 import serial
 import time
 import can
 import threading
 import subprocess
 import time
-#import RPi.GPIO as GPIO
+from gpiozero import LED
+
+multi_meter_id = "5491B  Multimeter,Ver1.1.11.11.23,124E12115"
+
+multi_meter = '/dev/ttyUSB0'
+e_load = '/dev/usbtmc0'
 
 can_channel = "can1"
 
 # Pin configuration
 K1_PIN = 26
+k1_pin = LED(26) # LED object for BCM pin 23
 
 # Setup
 #GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
 #GPIO.setup(K1_PIN, GPIO.OUT)
-#GPIO.output(K1_PIN, GPIO.HIGH)   # Turn LED off
+#GPIO.output(K1_PIN, GPIO.LOW)   # Turn LED off
 
 RLY_CTRL_ID = 0x0CFF0500
 K1State = 0
 
 # Create a stop event for clean thread shutdown
 stop_event = threading.Event()
-
 
 # Function to continuously receive CAN messages in a thread
 def receive_can_messages():
@@ -38,9 +44,9 @@ def receive_can_messages():
             else:
                 K1State = 0
             if(K1State):
-                d=1#GPIO.output(K1_PIN, GPIO.LOW)  # Turn LED on
+                k1_pin.on()#GPIO.output(K1_PIN, GPIO.HIGH)  # Turn LED on
             else:
-                d=0#GPIO.output(K1_PIN, GPIO.HIGH)   # Turn LED off
+                k1_pin.off()#GPIO.output(K1_PIN, GPIO.LOW)   # Turn LED off
 
 def is_number(value):
     try:
@@ -58,41 +64,51 @@ try:
     
 except subprocess.CalledProcessError as e:
     print(f"Failed to bring up CAN interface: {e}")
-    
+
+baud=38400
+response = ""
+try:
+    mmeter = serial.Serial(multi_meter, baud)
+    mmeter.write(b'*IDN?\n')
+    while(response == ""):
+        time.sleep(0.15)
+        response = mmeter.readline().decode().strip()
+        print(response)
+except:
+    mmeter = ""
+if not (mmeter):
+     print("Failed to communcate with multi meter")
+
 #cbus = can.interface.Bus(interface='pcan', bitrate=250000)
 cbus = can.interface.Bus(interface='socketcan', channel=can_channel, bitrate=250000)
-baud=38400
-ser = serial.Serial('/dev/ttyUSB0', baud)
-time.sleep(2)
-
 receiver_thread = threading.Thread(target=receive_can_messages, daemon=True)
 receiver_thread.start()
 
 try:
     while(1):
-        ser.write(b'FETC?\n')
-        #response = ser.readline().decode().strip()
-        response = ""
-        while(response == ""):
-            time.sleep(0.15)
-            response = ser.readline().decode().strip()
-        if is_number(response):
-            value = float(response)
-            print(value)
-            if(value<0):
-                value = 0
-            ivalue = value*1000
-            HB = int(ivalue % 256)
-            LB = int(ivalue / 256)
-            msg = can.Message(
-                arbitration_id=0x0CFF0004,
-                data=[HB, LB, 0, 0, 0, 0, 0, 0],
-                is_extended_id=True
-            )
-            try:
-                cbus.send(msg)
-            except can.CanError:
-                print("Message NOT sent")
+        if (mmeter):
+            mmeter.write(b'FETC?\n')
+            response = ""
+            while(response == ""):
+                time.sleep(0.15)
+                response = mmeter.readline().decode().strip()
+            if is_number(response):
+                value = float(response)
+                print(value)
+                if(value<0):
+                    value = 0
+                ivalue = value*1000
+                HB = int(ivalue % 256)
+                LB = int(ivalue / 256)
+                msg = can.Message(
+                    arbitration_id=0x0CFF0004,
+                    data=[HB, LB, 0, 0, 0, 0, 0, 0],
+                    is_extended_id=True
+                )
+                try:
+                    cbus.send(msg)
+                except can.CanError:
+                    print("Message NOT sent")
 
 except KeyboardInterrupt:
     print("Interrupted by user.")
@@ -103,8 +119,9 @@ finally:
     receiver_thread.join()
     print("shutdown canbus...")
     cbus.shutdown()  # Properly closes the interface
-    print("close serial...")
-    ser.close()
+    if (mmeter):
+        print("close meter...")
+        mmeter.close()
     print("bring down CAN interface...")
     try:
         subprocess.run(["sudo", "ip", "link", "set", can_channel, "down"], check=True)
